9379191b6406748099f760dc11ea318c6855ae28
stark-net <erfanf9101@gmail.com>
2025-03-29 15:03:38 +0330

    dou file removed

--- dou.py
+++
@@ -1,9 +0,0 @@
-from playsound import playsound
-first = int(input("Enter a NUM: "))
-second = int(input("Enter a NUM: "))
-if first == second:
-    playsound('/media/stark/Local_Disk/Music/15_not_afraid.mp3')
-else:
-    s = str(input("Enter a string: "))
-    S = s[len(s)/2]-1+s[len(s)+2]
-    print(S)
