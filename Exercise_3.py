4ef9a6e80441b6a01e03e34b1c92ac5f6a88bef1
stark-net <erfanf9101@gmail.com>
2025-04-03 02:16:16 +0330

    Code edited. It's final code.

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,34 +1,42 @@
-n = int(input("Enter number of the columns of matrix: "))
-m = [[0] * n for _ in range(n)]
-for row in range(n):
-    while(True):
-        try:
-            value = input(f"Enter value of row ({row}): ")
-            m[row] = value
-            break
-        except:
-            print("Enter number in correct form(1 or 0)")
+def is_even(x):
+    if x % 2 == 0:
+        return True
+    else:
+        return False
+    
+n = int(input("Enter number of the columns of matrix: "))                                                                                                                                                                                     
+m = [[0] * n for _ in range(n)]   
+sum_of_row = []    
+m_int = []                                                                                                                                                                                                        
 
-for j in m:
-    for i in j:
-        Integer_value_of_i = int(i)
-        if Integer_value_of_i in [1,0]:
-            pass
-        else:
-            print("Your entered number is not binary")
+for row in range(n):                                                                                                                                                                                                                          
+    while(True):                                                                                                                                                                                                                              
+        try:                                                                                                                                                                                                                                  
+            value = input(f"Enter value of row {row} (only 1 and 0, without spaces): ")                                                                                                                                                       
+            if any(i not in "01" for i in value) or len(value) != n:                                                                                                                                                                          
+                raise ValueError("\tYour entered number is not valid. Please make sure about it.")                                                                                                                                              
+            m[row] = [int(i) for i in value]                                                                                                                                                                                                  
+            break                                                                                                                                                                                                                             
+        except ValueError as error:                                                                                                                                                                                                           
+            print(error)                                                                                                                                                                                                                      
+    if value == 0:                                                                                                                                                                                                                           
+        break
 
-for j in m:
-    for i in j:
-        Integer_value_of_i = int(i)
-        if Integer_value_of_i == 1:
-            c++
+'''for i in range(len(m)):
+    sum_of_index = 0
+    for j in range(len(m[i])):
+        sum_of_index += int(m[i][j])
+    sum_of_row.append([sum_of_index])'''
+    
+    
+sum_of_row = [sum(row) for row in m]
+
+print("OK" if all(map(is_even, sum_of_row)) else "Corrupt")
 
 
 
-
-
-else:
-    for j in m:
-        for i in j:
-            print(i, end=' ')
-        print()
+'''print("\nThe matrix is:\n")                                                                                                                                                                                                                                    
+for j in m:                                                                                                                                                                                                                               
+    for i in j:                                                                                                                                                                                                                           
+        print(i, end=' ')                                                                                                                                                                                                                 
+    print()'''
