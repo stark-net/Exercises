6e9b41fd2a417978b3be4fd8703104a37cd3caec
stark-net <erfanf9101@gmail.com>
2025-04-05 02:58:10 +0330

    Exercise_1 added

---
+++ Exercise_1.py
@@ -0,0 +1,34 @@
+key = int(input("Enter shift amount (number): "))
+print("Enter your text (at most 100 characters)")
+print("Enter 0 in a new line to stop:\n")
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+total_length = 0
+lines = []
+while True:
+    line = input() 
+    if line == "0":
+        break
+    
+    total_length += len(line)
+    if total_length > 100:
+        print("Your input is too long")
+    
+    lines.append(line)
+
+plain_text = "\n".join(lines)
+
+ciphered_text = ""
+    
+for char in plain_text:
+    if char.isalpha():
+        upper_char = char.upper()
+        index = alphabet.index(upper_char)
+        new_index = (index + key) % 26
+        ciphered_char = alphabet[new_index]
+        ciphered_text += ciphered_char
+    else:
+        ciphered_text += char
+print("\nYour ciphered text is:\n"+          ciphered_text)
+        
+            
